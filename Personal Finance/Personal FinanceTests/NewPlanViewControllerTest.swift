1b003c62f926c8ec98d85bade37ce13a5ab1189f
Maysam Mohsenian <you@example.com>
2016-09-13 13:57:43 +0430

    OK

---
+++ Personal Finance/Personal FinanceTests/NewPlanViewControllerTest.swift
@@ -0,0 +1,27 @@
+//
+//  NewPlanViewControllerTest.swift
+//  Personal Finance
+//
+//  Created by Maysam Mohsenian on 6/23/1395 AP.
+//  Copyright © 1395 Mohammad Lashgarbolouk. All rights reserved.
+//
+
+import XCTest
+import UIKit
+
+
+class NewPlanViewControllerTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+
+    
+}
