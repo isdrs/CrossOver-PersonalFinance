23ff12a8ce20df5c847e523adb11ca9ca328606c
Maysam Mohsenian <you@example.com>
2016-09-14 01:07:28 +0430

    ]«

--- Personal Finance/Personal FinanceTests/FinanceControllerTest.swift
+++ Personal Finance/Personal FinanceTests/FinanceControllerTest.swift
@@ -20,6 +20,130 @@ class FinanceControllerTest: XCTestCase {
     {
         XCTAssertTrue(FinanceController.AddCategory("TestCat", _catType: PlanType.Expense))
     }
+
+
+    func testAddBankAccount()
+    {
+        XCTAssertTrue(FinanceController.AddBankAccount( "CrossOver" ,_accountNumber: "123431"))
+    }
+
+    func testUpdateBalance()
+    {
+        XCTAssertEqual(FinanceController.UpdateBalance("123431"),AccountManager.staticBalance)
+    }
+
+//    func testGetAccount()  {
+//
+//    }
+
+
+//    func testGetIncomePlans()
+//    {
+//        let incomePlans = PlanManager.GetPlansByType(PlanType.Income)
+//
+//        return incomePlans
+//    }
+
+//    func testGetExpensePlans()
+//    {
+//        let expensePlans = PlanManager.GetPlansByType(PlanType.Expense)
+//
+//        return expensePlans
+//    }
+
+    func testDeletePlan()
+    {
+        XCTAssertTrue(FinanceController.DeletePlan("1232322"))
+
+    }
+
+//    func testGetExpenseCategory()
+//    {
+//        let expenseCats = CategoryManager.GetCategoryByType(PlanType.Expense)
+//        return expenseCats
+//    }
+
+//    func testGetIncomeCategory()
+//    {
+//        let incomeCats = CategoryManager.GetCategoryByType(PlanType.Income)
+//        return incomeCats
+//    }
+
+    func testAddCategory()
+    {
+        XCTAssertTrue(FinanceController.AddCategory("c1", _catType: PlanType.Expense))
+    }
+
+    func testDeleteCategory(_id:Int) -> Bool
+    {
+        let cat = CategoryManager.GetCategoryById(_id)
+
+        let res = CategoryManager.DeleteCategory(cat)
+
+        return res
+    }
+
+    func testGetCategoriesByType(_type:PlanType) -> [CategoryItem]
+    {
+
+        return CategoryManager.GetCategoryByType(_type)
+    }
+
+    func testGetCategoryById(_catId:Int) -> CategoryItem
+    {
+        return CategoryManager.GetCategoryById(_catId)
+    }
+
+    func testAddPlan(_name:String,_amount:Double,_repeatitiontype:PlanRepetitionType,
+                        _repeatNumber:Int,_firstDate:NSDate,_cat:CategoryItem) -> Bool
+    {
+        let newPlan = PlanItem()
+        newPlan.Name = _name
+        newPlan.Amount = _amount
+        newPlan.RepeatitionType = _repeatitiontype
+        newPlan.RepeatNumber = _repeatNumber
+        newPlan.FirstTimeDate = _firstDate
+
+        newPlan.PlanCategory = _cat
+
+        if PlanManager.AddPlan(newPlan)
+        {
+            return true
+        }
+        return false
+    }
+
+
+    func testGetExpenseTotalEstimateAmountInCategory(_unitl:NSDate) -> [String:Double]
+    {
+        let totalExpense = AccountManager.GetTotalStimatedExpenenInCategory(_unitl)
+
+        return totalExpense
+
+    }
+
+    func testGetIncomeTotalEstimateAmountInCategory(_unitl:NSDate) -> [String:Double]
+    {
+        let totalIncomes = AccountManager.GetTotalStimatedIncomeInCategory(_unitl)
+
+        return totalIncomes
+    }
+
+    func testGetTotalEstimateBalance(_until:NSDate) -> Double
+    {
+        let estimatedBalance = AccountManager.GetEstimateAccountBalance(_until)
+        
+        return estimatedBalance
+    }
+    
+    func testUpdateAllList()
+    {
+        PlanManager.UpdatePlans()
+        
+        CategoryManager.UpdateCategories()
+    }
+
+
     
     
     override func setUp() {
@@ -32,16 +156,6 @@ class FinanceControllerTest: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
     
 }
